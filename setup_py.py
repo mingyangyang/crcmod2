e3159fcbdb676a1ea2fbf9c5ebd2f25279b8a5d1
Craig McQueen <craig@mcqueen.id.au>
2010-05-16 11:35:23 +0900

    Add Topic::Communications PyPI category to the setup files.

---
+++ setup_py.py
@@ -0,0 +1,43 @@
+from distutils.core import setup
+import sys
+
+if sys.version_info[0] == 2:
+    base_dir = 'py2'
+elif sys.version_info[0] == 3:
+    base_dir = 'py3'
+
+setup(
+name='crcmod',
+version='1.6.1',
+description='CRC Generator',
+author='Ray Buvel',
+author_email='rlbuvel@gmail.com',
+url='http://crcmod.sourceforge.net/',
+packages=['crcmod'],
+package_dir={
+    'crcmod' : base_dir + '/crcmod',
+},
+
+long_description=open('README').read(),
+
+license="MIT",
+classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Information Technology',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: C',
+    'Programming Language :: C++',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Topic :: Communications',
+    'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Utilities',
+],
+)
